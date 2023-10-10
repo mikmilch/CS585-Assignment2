255de6b222f8177263dc45f380c095154eda2275
Nicholas Li <58043224+Livingwell088@users.noreply.github.com>
2023-10-10 10:45:31 -0400

    Kmeans dataset creation for BYOD data

---
+++ src/main/python/BYODKmeans.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import random
+
+
+features = [
+    "x",
+    "y"
+]
+BYOD_kmeans_dataset = pd.DataFrame(columns=features)
+
+dups = []
+x_list = []
+y_list = []
+def check_dups(x, y):
+    if ("x y" in dups):
+        x = random.randint(0, 150)
+        y = random.randint(0, 100)
+        check_dups(x, y)
+    else:
+        dups.append(str(x) + " " + str(y))
+        x_list.append(x)
+        y_list.append(y)
+
+
+def kmeans(k):
+    for i in range(k):
+        x = random.randint(0, 150)
+        y = random.randint(0, 100)
+
+        check_dups(x, y)
+
+    BYOD_kmeans_dataset["x"] = x_list
+    BYOD_kmeans_dataset["y"] = y_list
+    # print(BYOD_kmeans_dataset)
+    BYOD_kmeans_dataset.to_csv("BYOD_kmeans_dataset.csv", index=False, header=None)
+
+
+if __name__ == '__main__':
+    kmeans(5)
